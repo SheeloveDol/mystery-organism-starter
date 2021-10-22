117f2634a381b90b9db146357872e65d2858c82a
Sheelove Dol <siesel26@gmail.com>
2021-10-22 15:40:27 -0700

    Add pAequorFactory factory function

--- main.js
+++ main.js
@@ -13,12 +13,24 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Creates new instances of objects
 const pAequorFactory = (number, array) => {
-  return 
+  return {
+    specipmen: number,
+    dna: array
+  }
 }
 
+const firstSpecies = pAequorFactory(1, mockUpStrand());
+console.log(firstSpecies)
 
-
-
+/* 
+1. add .mutate() to pAequorFactory
+2. make it randomly select a base from the dna property array
+    a) create a variable to store the array's length
+    b) use this variable to create another variable that selects a random number from that length
+3. use random number to pick a random DNA base from the dna array
+4. create a new array
+*/
 
 
